822382d911323b3afdda865ce71fb54de73e2b61
skwerlman <skwerlman@users.noreply.github.com>
2024-01-23 22:40:28 -0500

    rename old options

--- config/users/sk/hyprland.nix
+++ config/users/sk/hyprland.nix
@@ -2,11 +2,11 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    systemdIntegration = true;
+    systemd.enable = true;
     xwayland = {
       enable = true;
     };
-    nvidiaPatches = false;
+    enableNvidiaPatches = false;
     extraConfig = builtins.readFile ../../../resources/conf/hyprland.conf +
       # ensure its always possible to logout
       "bind=SUPER_SHIFT,Q,exec,${pkgs.wlogout}/bin/wlogout\n";
