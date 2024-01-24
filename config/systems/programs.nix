85b95530d858435a91e4245f2d131c1b58338c71
skwerlman <skwerlman@users.noreply.github.com>
2024-01-24 12:49:34 -0500

    try theming regreet

--- config/systems/programs.nix
+++ config/systems/programs.nix
@@ -3,6 +3,15 @@
   programs = {
     regreet = {
       enable = true;
+      settings = {
+        background = {
+          path = ../../resources/wallpaper/ludens.jpg;
+        };
+        GTK = {
+          application_prefer_dark_theme = true;
+          theme_name = "Stylix";
+        };
+      };
     };
   };
 }
