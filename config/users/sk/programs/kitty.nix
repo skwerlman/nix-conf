b90f1cb6dc89f8ff49ce26a8d67eaa18b734976f
skwerlman <skwerlman@users.noreply.github.com>
2024-01-23 22:58:22 -0500

    dont set font in kitty config

--- config/users/sk/programs/kitty.nix
+++ config/users/sk/programs/kitty.nix
@@ -1,10 +1,6 @@
 {
   programs.kitty = {
     enable = true;
-    font = {
-      name = "FiraCode Nerd Font Mono";
-      size = 11;
-    };
     settings = {
       disable_ligatures = "never";
     };
