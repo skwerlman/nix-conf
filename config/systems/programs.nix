75b50df69866eaa8e9edbb521695756ac17f5854
skwerlman <skwerlman@users.noreply.github.com>
2024-01-24 13:05:41 -0500

    tell regreet where to find sessions; try setting theme in a more flexible way

--- config/systems/programs.nix
+++ config/systems/programs.nix
@@ -1,15 +1,25 @@
-{ config, pkgs, ... }:
+{ config, pkgs, common-attrs, ... }:
+let
+  regreet-override = pkgs.greetd.regreet.overrideAttrs (final: prev: {
+    SESSION_DIRS = "${config.services.xserver.displayManager.sessionData.desktops}/share";
+  });
+in
 {
   programs = {
     regreet = {
+      package = regreet-override;
       enable = true;
       settings = {
         background = {
           path = ../../resources/wallpaper/ludens.jpg;
+          fit = "Cover";
         };
         GTK = {
           application_prefer_dark_theme = true;
-          theme_name = "Stylix";
+          theme_name = common-attrs.gtk-theme.cursorTheme.name;
+          icon_theme_name = common-attrs.gtk-theme.cursorTheme.name;
+          cursor_theme_name = common-attrs.gtk-theme.cursorTheme.name;
+          font_name = "${common-attrs.gtk-theme.font.name} 12";
         };
       };
     };
