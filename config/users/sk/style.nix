0bfba79cb1c1b52b8b5ce73e89fea1ddc8f3a493
skwerlman <skwerlman@users.noreply.github.com>
2024-01-25 12:53:59 -0500

    theme gtk

--- config/users/sk/style.nix
+++ config/users/sk/style.nix
@@ -1,8 +1,18 @@
+{ pkgs, ... }:
 {
   stylix = {
     targets = {
       kitty.variant256Colors = true;
       swaylock.useImage = true;
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = { name = "Stylix"; };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
   };
 }
